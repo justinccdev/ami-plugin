34301c43955e3b7771dd81e15cfd13706a6af988
ContentMine Workshop <workshop@crunchbang.(none)>
2015-12-11 13:59:59 +0000

    Count number of results and print out for each file and at end

--- ami-regex-concat.py
+++ ami-regex-concat.py
@@ -22,6 +22,7 @@ parser.add_argument('amiResultsPath', help='path to ami2-regex results.')
 parser.add_argument('outFile', nargs='?', type=argparse.FileType('w'))
 args = parser.parse_args()
 
+count = 0
 out = ""
 
 for root, dirs, files in os.walk(args.amiResultsPath):
@@ -30,7 +31,13 @@ for root, dirs, files in os.walk(args.amiResultsPath):
         if 'regex' in path:
             out = out + "### From %s ###\n" % path
             tree = ET.parse(path)
+            results = tree.findall('result')
+            resultsCount = len(results)
+            count += resultsCount
+            out = out + "%d results\n" % resultsCount
             out = out + ET.tostring(tree.getroot()) + "\n"
+
+out = out + "\nSUMMARY\n%d results" % count
 
 if args.outFile:
   args.outFile.write(out)
